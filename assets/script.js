c067d1c71772220bdc00ed9c796eea80ecd0749e
Carson <carsoncolgateccu@gmail.com>
2021-09-29 14:41:10 -0600

    fixed a bug that was saving descriptions to all my hours not just 1

--- assets/script.js
+++ assets/script.js
@@ -12,11 +12,11 @@ $(".saveBtn").on("click", function () {
 })
 
 $("#hour9 .description").val(localStorage.getItem("hour9"));
-$("#hour10 .description").val(localStorage.getItem("hour9"));
-$("#hour11 .description").val(localStorage.getItem("hour9"));
-$("#hour12 .description").val(localStorage.getItem("hour9"));
-$("#hour1 .description").val(localStorage.getItem("hour9"));
-$("#hour2 .description").val(localStorage.getItem("hour9"));
-$("#hour3 .description").val(localStorage.getItem("hour9"));
-$("#hour4 .description").val(localStorage.getItem("hour9"));
-$("#hour5 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour1 .description").val(localStorage.getItem("hour1"));
+$("#hour2 .description").val(localStorage.getItem("hour2"));
+$("#hour3 .description").val(localStorage.getItem("hour3"));
+$("#hour4 .description").val(localStorage.getItem("hour4"));
+$("#hour5 .description").val(localStorage.getItem("hour5"));
