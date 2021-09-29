d72b75368c4013e64994663c29898228bcdf3db1
Carson <carsoncolgateccu@gmail.com>
2021-09-29 14:38:13 -0600

    added variable that saves my time as well then added a local storage for each block to save

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,19 @@ var formattedToday = today.format("dddd, MMMM Do");
 $("#currentDay").text(formattedToday);
 
 $(".saveBtn").on("click", function () {
-    console.log(description);
-    var text = $(description).siblings(".description").val();
-    localStorage.setItem(time, text)
+    //get description and time.
+    console.log(this);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
 })
+
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour9"));
+$("#hour11 .description").val(localStorage.getItem("hour9"));
+$("#hour12 .description").val(localStorage.getItem("hour9"));
+$("#hour1 .description").val(localStorage.getItem("hour9"));
+$("#hour2 .description").val(localStorage.getItem("hour9"));
+$("#hour3 .description").val(localStorage.getItem("hour9"));
+$("#hour4 .description").val(localStorage.getItem("hour9"));
+$("#hour5 .description").val(localStorage.getItem("hour9"));
