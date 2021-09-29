add4003788efa8abfe25e1900a4354a033176274
Carson <carsoncolgateccu@gmail.com>
2021-09-29 14:52:51 -0600

    tried adding if statements for time to show color changes didnt work

--- assets/script.js
+++ assets/script.js
@@ -22,4 +22,50 @@ $("#hour4 .description").val(localStorage.getItem("hour4"));
 $("#hour5 .description").val(localStorage.getItem("hour5"));
 
 // reads hours in military time
-var currentHour = Number(currentDay.format("H"));
+var currentTime = Number(currentDay.format("H"));
+
+if (currentTime === 9) {
+    $("#0").addClass("present");
+} else if (currentTime > 9) {
+    $("#0").addClass("past");
+};
+if (currentTime === 10) {
+    $("#1").addClass("present");
+} else if (currentTime > 10) {
+    $("#1").addClass("past");
+};
+if (currentTime === 11) {
+    $("#2").addClass("present");
+} else if (currentTime > 11) {
+    $("#2").addClass("past");
+};
+if (currentTime === 12) {
+    $("#3").addClass("present");
+} else if (currentTime > 12) {
+    $("#3").addClass("past");
+};
+if (currentTime === 13) {
+    $("#4").addClass("present");
+} else if (currentTime > 13) {
+    $("#4").addClass("past");
+};
+if (currentTime === 14) {
+    $("#5").addClass("present");
+} else if (currentTime > 14) {
+    $("#5").addClass("past");
+};
+if (currentTime === 15) {
+    $("#6").addClass("present");
+} else if (currentTime > 15) {
+    $("#6").addClass("past");
+};
+if (currentTime === 16) {
+    $("#7").addClass("present");
+} else if (currentTime > 16) {
+    $("#7").addClass("past");
+};
+if (currentTime === 17) {
+    $("#8").addClass("present");
+} else if (currentTime > 17) {
+    $("#8").addClass("past");
+};
