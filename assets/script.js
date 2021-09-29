9eb85ec4bfb8e4ac0ca465d1450cc7b36a7528b6
Carson <carsoncolgateccu@gmail.com>
2021-09-29 12:17:39 -0600

    styled my page using bootstrap to create my rows and save buttons

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,9 @@
 var today = moment();
-var formattedToday = today.format("dddd,MMMM Mo");
-$("currentDay").text(formattedToday);
+// var formattedToday = today.format("dddd,MMMM Mo");
+// $("currentDay").text(formattedToday);
+
+function checkTime() {
+    const todaysDate = document.querySelector("#currentDay");
+    var currentTime = moment();
+    todaysDate.textContent = currentTime.format("dddd, MMMM Do, YYYY");
+}
