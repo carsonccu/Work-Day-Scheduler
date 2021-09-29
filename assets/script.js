9912d7e2bcc2e4798ce7675143421873ed1acc66
Carson <carsoncolgateccu@gmail.com>
2021-09-29 12:44:42 -0600

    added a variable to format my time in 24 hours to work on color changer

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
-
+// displays the date in the header
 var today = moment();
-var formattedToday = today.format("dddd,MMMM Mo");
+var formattedToday = today.format("dddd, MMMM Mo");
 $("#currentDay").text(formattedToday);
+
+var currentTime = moment().format("H");
