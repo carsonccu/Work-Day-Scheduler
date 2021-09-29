5c138395780795ff7868a6398bd508d96dbd11d4
Carson <carsoncolgateccu@gmail.com>
2021-09-29 14:16:43 -0600

    added a listener to my save button that will hopefully save my description of my to do by grabbing the sibling from each block

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,9 @@
 var today = moment();
 var formattedToday = today.format("dddd, MMMM Do");
 $("#currentDay").text(formattedToday);
-// var to format time to 24 hour 
 
-let currentTime = moment().hour("H");
-console.log()
-
-function timeCompare() {
-    var blockhour = parseInt($(this).attr("id").split("hour")[1]);
-    console.log(blockhour, currentTime);
-}
+$(".saveBtn").on("click", function () {
+    console.log(description);
+    var text = $(description).siblings(".description").val();
+    localStorage.setItem(time, text)
+})
