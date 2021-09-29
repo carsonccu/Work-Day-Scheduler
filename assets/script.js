1df853b3aed6ac7b128dcb6ec21da4ff78565a90
Carson <carsoncolgateccu@gmail.com>
2021-09-29 12:49:04 -0600

    added variable for my textbox

--- assets/script.js
+++ assets/script.js
@@ -2,5 +2,6 @@
 var today = moment();
 var formattedToday = today.format("dddd, MMMM Mo");
 $("#currentDay").text(formattedToday);
-
+// var to format time to 24 hour 
 var currentTime = moment().format("H");
+var textbox = $(".col-md-10 description");
