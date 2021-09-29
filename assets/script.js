d37fd7952a0bc43c15b1f3a30720e805fe0c5c0a
Carson <carsoncolgateccu@gmail.com>
2021-09-29 12:27:55 -0600

    got my date to display in my header

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,4 @@
-var today = moment();
-// var formattedToday = today.format("dddd,MMMM Mo");
-// $("currentDay").text(formattedToday);
 
-function checkTime() {
-    const todaysDate = document.querySelector("#currentDay");
-    var currentTime = moment();
-    todaysDate.textContent = currentTime.format("dddd, MMMM Do, YYYY");
-}
+var today = moment();
+var formattedToday = today.format("dddd,MMMM Mo");
+$("#currentDay").text(formattedToday);
